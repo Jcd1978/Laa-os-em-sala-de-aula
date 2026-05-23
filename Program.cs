d4c41e437349413532fb8949f31ac5cc6f49e482
Joelma <joelma.carmo.duarte@gmail.com>
2026-05-22 21:46:39 -0300

    Exercício concluído

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Write("Quantas vezes você quer exibir? ");
+int maximo = Convert.ToInt32(Console.ReadLine());
+
+int contador = 0;
+while(contador < maximo)
+{
+    if (contador % 2 == 0)
+    {
+        Console.BackgroundColor = ConsoleColor.Yellow;
+    }
+else
+    {
+        Console.BackgroundColor = ConsoleColor.Blue;
+    }
+
+    int repeticoes = 0;
+    while(repeticoes < 3)
+    {
+        Console.Write("Hello, World! ");
+        repeticoes = repeticoes + 1;
+    }
+
+    contador = contador + 1;
+    Console.WriteLine(contador);
+     
+}
+Console.ResetColor();
